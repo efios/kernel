b1eb933a8683743806d2db45c46f3d95ed24045c
efios <imtkyo@gmail.com>
2017-06-15 01:58:16 +0200

    removed some magic numbers

--- kernel.c
+++ kernel.c
@@ -1,16 +1,26 @@
+/* kernel .c */
 /* gcc -m32 -c kernel.c -o kernelc.o */
+
+#define VIDEO_START 0xb8000		       
+#define MAX_LINES 25
+#define CHARACTERS_PER_LINE 80
+#define BYTES_PER_ELEMENT 2
+
+#define LIGHT_GRAY 0x07
+
+void kernel_clear_screen() { }
 
 void kernel_main()
 {
     const char *string = "My first kernel!";
-    char *vidptr = (char*)0xb8000;
+    char *vidptr = (char*)VIDEO_START;
 
     unsigned int i = 0, j = 0, k = 0;
 
-    while(j < 80 * 25 * 2)
+    while(j < CHARACTERS_PER_LINE * MAX_LINES * BYTES_PER_ELEMENT)
     {
         vidptr[j] = ' ';
-        vidptr[j + 1] = 0x07;
+        vidptr[j + 1] = LIGHT_GRAY;
         j = j + 2;
     }
   
@@ -18,7 +28,7 @@ void kernel_main()
     {
         vidptr[i] = string[k];
 
-        vidptr[i + 1] = 0x07;
+        vidptr[i + 1] = LIGHT_GRAY;
         i = i + 2;
     }
     return;
