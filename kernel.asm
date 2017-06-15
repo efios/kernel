665fd9ba5d1f6b2c245d10b153afd51e7a6330fc
efios <imtkyo@gmail.com>
2017-06-15 02:34:48 +0200

    IDT

--- kernel.asm
+++ kernel.asm
@@ -11,7 +11,22 @@ section .text
     dd - (0x1BADB002 + 0x00)
 
 global start
+global read_port
+global write_port
+
+	
 extern kernel_main
+
+read_port:
+	mov edx, [esp + 4]
+	in al, dx
+	ret
+	
+write_port:
+	mov edx, [esp + 4]
+	mov al, [esp + 4 + 4]
+	out dx, al
+	ret
 
 start:
     cli				; block interrupts
